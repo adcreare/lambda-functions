88ddc4d14591f84e16558bda622b015bdee5bd2c
David <caveman@commscentral.net>
2017-07-28 14:25:35 -0400

    package and other basic items completed

--- ami-creator/ami-creator-engine.js
+++ ami-creator/ami-creator-engine.js
@@ -1,16 +1,21 @@
 'use strict';
 
 module.exports.handlerequest = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
 
-  callback(null, response);
+  // TODO: tasks we need to perform
+  // 1. check params - error if none exist
+  // 2. download command file from S3
+  // 3. check instance exists
+  // 4. check instance is in a shutdown state (tied to 3 maybe)
+  // 5. take AMI snapshot
+  //
+  // OPTIONAL:
+  // 6. start state machine
+  //
+  //
+  // TODO statemachine OPTIONAL
+  // 1. check if ami snap has been completed - if not sleep and retry
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
