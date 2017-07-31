9b9697451bb7b2c5bb077ff139a38a5017855951
David <caveman@commscentral.net>
2017-07-31 12:16:10 -0400

    unzip fixed, tests added for most of the code base - ready for first testing

--- ami-creator/ami-creator-engine.js
+++ ami-creator/ami-creator-engine.js
@@ -1,8 +1,12 @@
 'use strict';
 const waterfall = require('async/waterfall');
 const AWS = require('aws-sdk');
+const AdmZip = require('adm-zip');
 const s3 = new AWS.S3();
-const zlib = require('zlib');
+const ec2 = new AWS.EC2();
+
+const snapShotDescription = 'snap taken by ami-creator-engine';
+const snapShotName = 'mytestsnapshot'
 
 module.exports.handlerequest = (event, context, callback) => {
 
@@ -26,24 +30,24 @@ module.exports.handlerequest = (event, context, callback) => {
   //
   const keyData = validateInputAndProvideKeyData(event);
   if (keyData === false) callback(null, { message: 'Incorrect input supplied: '+JSON.stringify(event) });
-  else performOperations(keyData,callback);
+  else runWorkFlow(keyData,callback);
 
 
 }; //end handlerequest
 
 
-function performOperations(inputObject,callback)
+function runWorkFlow(inputObject,callback)
 {
   waterfall(
     [
       getFileFromS3(inputObject.bucketname,inputObject.objectkey),
       extractFile,
+      processCFStackResponse,
+      createAMIMachineImage(snapShotName,snapShotDescription)
 
-      testImDoneFunction
   ],
   function (err, message) {
     console.log('Error received - error object: '+JSON.stringify(err)+' - message: '+message);
-    // Node.js and JavaScript Rock!
    }
   );
 }
@@ -71,34 +75,37 @@ function getFileFromS3(bucket,key,getfileCallback)
 //Function to extract a zip file
 function extractFile(buffer,extractFileCallBack)
 {
-  zlib.gunzip(buffer,extractFileCallBack);
+  var zip = new AdmZip(buffer);
+  var zipEntries = zip.getEntries();
+
+  if (zipEntries.length != 1) throw new Error('Zip file appears to have multiple files instead of the expected 1');
+
+  extractFileCallBack(zip.readAsText(zipEntries[0]));
 }
 
-function processCFStackResponse(test)
+function processCFStackResponse(dataContainedInFile)
 {
-  return test;
+  return JSON.parse(dataContainedInFile);
 }
 
-// //Callback for file extraction
-// function extractFileCallBack(error,result)
-// {
-//   if (error){
-//       global.lambdaCallback(error,'unable to extract file!') //exit function
-//   }
-//   else
-//   {
-//       loadIntoES(JSON.parse(result.toString())); //now load the data into ES
-//   }
-// }
-
+function createAMIMachineImage(instanceId,snapshotName,snapShotDescription, callback)
+{
+  const params = {
+    InstanceId: 'STRING_VALUE', /* required */
+    Name: 'STRING_VALUE', /* required */
+    Description: 'STRING_VALUE',
+  };
+  ec2.createImage(params, function(err, data) {
+    if (err) callback(err); // an error occurred
+    else callback(null,data);           // successful response
+  });
+}
 
 /*
 
  */
 function validateInputAndProvideKeyData(event)
 {
-  // console.log(JSON.stringify(event));
-  //check we have an S3 location
 
   let bucketName, objectKey;
 
@@ -110,7 +117,5 @@ function validateInputAndProvideKeyData(event)
   {
     return false;
   }
-
   return {'bucketname':bucketName,'objectkey':objectKey};
-
 }
--- ami-creator/tests/unit_ami_creator_engine.js
+++ ami-creator/tests/unit_ami_creator_engine.js
@@ -2,21 +2,32 @@
 var assert = require("chai").assert;
 var expect = require("chai").expect;
 var rewire = require('rewire');
+const fs = require('fs');
 
 var myCodeFile = rewire('../ami-creator-engine.js');
 
 const testEvent = require('../sample-events/codepipeline-events.json');
 
+var path = process.cwd();
+const testEventMyStackFileOutput = fs.readFileSync(path+'/sample-events/MyStackOutputFileName');
+const testEventMyStackFileOutputZipped = fs.readFileSync(path+'/sample-events/stackoutput.zip');
+const testEventMyStackFileOutputZippedBadFile = fs.readFileSync(path+'/sample-events/stackoutput-badfile.zip');
 
-var performOperations = myCodeFile.__get__('performOperations');
-
+var runWorkFlow = myCodeFile.__get__('runWorkFlow');
 var validateInput = myCodeFile.__get__('validateInputAndProvideKeyData');
+var extractFile = myCodeFile.__get__('extractFile');
+var getFileFromS3 = myCodeFile.__get__('getFileFromS3');
+var processCFStackResponse = myCodeFile.__get__('processCFStackResponse');
+var createAMIMachineImage = myCodeFile.__get__('createAMIMachineImage');
 
 describe("main function file", function() {
 
   it("exports as a function", function() {
       assert.typeOf(validateInput, "function");
-      assert.typeOf(performOperations, "function");
+      assert.typeOf(runWorkFlow, "function");
+      assert.typeOf(getFileFromS3, "function");
+      assert.typeOf(processCFStackResponse, "function");
+      assert.typeOf(createAMIMachineImage, "function");
   });
 
   it("validateInput works as expected", function() {
@@ -25,7 +36,29 @@ describe("main function file", function() {
 
   });
 
-  
+  it("extractFile works as expected to extract good file", function(done) {
+    extractFile(testEventMyStackFileOutputZipped, function(data){
+      assert.typeOf(data,"string");
+      done();
+    })
+  });
+
+  it("extractFile throws as expected with bad file input", function(done) {
+    assert.throws(function(){
+      extractFile(testEventMyStackFileOutputZippedBadFile,function(data){})
+    }, Error, 'Zip file appears to have multiple files instead of the expected 1');
+    done();
+  });
+
+  it("processCFStackResponse works as expected", function() {
+    expect(processCFStackResponse(testEventMyStackFileOutput)).to.deep.equal({"InstanceID": "i-0f6d82a4ab76ce609"});
+  });
+
+
+  it("processCFStackResponse works as expected", function() {
+    expect(processCFStackResponse('{"InstanceID":"i-0f6d82a4ab76ce609"}')).to.deep.equal({InstanceID:'i-0f6d82a4ab76ce609'});
+  });
+
 
 
 });
